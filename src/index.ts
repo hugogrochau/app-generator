a0dc2da4b5096c369d58682d17b0485e8ba9cf35
Hugo Grochau <hugo@grochau.com>
2018-08-04 18:14:04 -0300

    Add commander.js for parsing cli args

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
 import { logger } from './logger'
+import commander from 'commander'
 
-logger.info('Hello world')
+const VERSION = '0.0.1'
+commander.version(VERSION)
+  .arguments('<input> [output]')
+  .option('-i --input <file>', 'Input template')
+  .option('-o --output <directory>', 'Destination directory')
+  .option('-v --verbose', 'Verbose log')
+  .parse(process.argv)
+
+console.log('commander.verbose', commander.verbose)
+if (commander.verbose) {
+  process.env.VERBOSE = 'true'
+}
+
+logger.debug(`Starting app generator v${VERSION}`)
+
+const input = commander.input || commander.args[0]
+const output = commander.output || commander.args[1] || 'dist'
+
+if (!input) {
+  logger.error('Input required')
+  commander.outputHelp()
+  process.exit(1)
+}
+
+console.log('output', output)
--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,9 @@
 import chalk from 'chalk'
 
+const isVerbose = () => process.env.VERBOSE === 'true'
+
 export const logger = {
-  debug: (message: string) => console.debug(`${chalk.bold.blue('DEBUG')} ${message}`),
+  debug: (message: string) => isVerbose() && console.debug(`${chalk.bold.blue('DEBUG')} ${message}`),
   info: (message: string) => console.info(`${chalk.bold.green('INFO')} ${message}`),
   warn: (message: string) => console.warn(`${chalk.bold.yellow('WARN')} ${message}`),
   error: (message: string) => console.error(`${chalk.bold.red('ERROR')} ${message}`)
