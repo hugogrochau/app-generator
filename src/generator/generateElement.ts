88e55712c3874032f4d52a445c20e2517d058d8d
Hugo Grochau <hugo@grochau.com>
2018-11-24 19:07:19 -0200

    Fix style generation

--- src/generator/generateElement.ts
+++ src/generator/generateElement.ts
@@ -2,10 +2,9 @@ import { Element, ComponentName } from '../types'
 import { executeTemplate } from '../templates'
 
 export const elementToComponentMap = {
+  [ComponentName.layoutBox]: 'Box',
   [ComponentName.uiText]: 'Text',
   [ComponentName.uiImage]: 'Image',
-  [ComponentName.layoutBox]: 'Box',
-
   [ComponentName.uiButton]: 'Button',
   [ComponentName.uiTextInput]: 'TextInput',
   [ComponentName.uiTextArea]: 'TextArea',
@@ -16,15 +15,16 @@ export const generateElement = (element: Element, depth: number): string => {
   const { name, props, style, children } = element
   const componentName = elementToComponentMap[name]
   const indentation = ''.padStart(depth * 2)
+  const formattedStyle = JSON.stringify(style)
 
   if (!children) {
-    return executeTemplate('leafElement', { name: componentName, props, style, indentation })
+    return executeTemplate('leafElement', { name: componentName, props, style: formattedStyle, indentation })
   }
 
   if (typeof children === 'string') {
-    return executeTemplate('elementWithChildren', { name: componentName, props, style, children: `  ${indentation}${children}`, indentation })
+    return executeTemplate('elementWithChildren', { name: componentName, props, style: formattedStyle, children: `  ${indentation}${children}`, indentation })
   }
 
   const generatedChildren = children.map(c => generateElement(c, depth + 1)).join('\n')
-  return executeTemplate('elementWithChildren', { name: componentName, props, style, children: generatedChildren, indentation })
+  return executeTemplate('elementWithChildren', { name: componentName, props, style: formattedStyle, children: generatedChildren, indentation })
 }
